2d300f0ab0b0c2a30b6bf3ddf9d7d8e443bdc7f1
wty <wty@WTY>
2017-11-14 16:13:24 +0800

    无关改动：方便测试多个句子的效率

--- src/Rete/main.cpp
+++ src/Rete/main.cpp
@@ -13,14 +13,19 @@ void test() {
 	COOVRecognitionByPattern::Init();
 	CLog::write("Init", "end");
 
+	CLog::write("Recog", "start");
 	COOVRecognitionByPattern rc;
 	std::vector<MatchResultNode> result;
-	rc.RecognitionByOOVPattern(result, {
-		"几", "个", "月", "不", "冲", "值", "，", "是否", "要", "去掉", "那", "几", "个", "没", 
+	std::vector<std::string> words = {
+		"几", "个", "月", "不", "冲", "值", "，", "是否", "要", "去掉", "那", "几", "个", "没",
 		"冲", "的", "月", "租", "？"
-	}, {
+	}, poses = {
 		"m", "q", "n", "d", "v", "n", "w", "v", "v", "v", "r", "m", "q", "d", "v", "uj", "n", "ng", "w"
-	});
+	};
+	for (size_t i = 0; i < 100; ++i) {
+		rc.RecognitionByOOVPattern(result, words, poses);
+	}
+	CLog::write("Recog", "end");
 	for (auto&& nd : result) {
 		debugIII(nd.nBegin, nd.nEnd, nd.matchedPattern);
 	}
